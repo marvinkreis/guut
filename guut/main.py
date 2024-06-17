2edbcbf3c39783776b04914e1e39492b45519df0
Marvin Kreis <MarvinKreis@web.de>
2024-06-17 13:54:55 +0200

    Add main code and some parsing

---
+++ guut/main.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+from llama_cpp import Llama
+from openai import OpenAI
+
+from guut.llm import OpenAIEndpoint, Conversation, LlamacppEndpoint, UserMessage
+from guut.log import log_conversation
+from guut.prompts import prompt
+from guut.quixbugs_helper import Problem, format_problem
+
+
+def main():
+    problem = get_problem()
+    conversation = prepare_conversation(problem)
+    endpoint = get_openai_endpoint()
+
+    print(repr(conversation))
+
+    response = endpoint.complete(conversation)
+    conversation.append(response)
+
+    print(repr(response))
+
+    log_conversation(conversation)
+
+
+def get_problem() -> Problem:
+    if len(sys.argv) != 2:
+        print("Please provide a QuixBugs problem name")
+
+    problem = Problem(sys.argv[1])
+    problem.validate()
+    return problem
+
+
+def prepare_conversation(problem: Problem):
+    prompt_instance = f'{prompt}\n{format_problem(problem)}'
+
+    return Conversation([
+        UserMessage(prompt_instance)
+    ])
+
+
+def get_llama_endpoint():
+    llama_path = os.environ['LLAMA_PATH']
+    client = Llama(
+        model_path=llama_path,
+        n_gpu_layers=-1, # Uncomment to use GPU acceleration
+        # seed=1337, # Uncomment to set a specific seed
+        # n_ctx=2048, # Uncomment to increase the context window
+    )
+    return LlamacppEndpoint(client)
+
+
+def get_openai_endpoint():
+    client = OpenAI()
+    return OpenAIEndpoint(client, 'gpt-3.5-turbo-0125')
---
+++ guut/parsing.py
@@ -0,0 +1,16 @@
+def extract_code(response: str, language: str):
+    taking_lines = False
+    code_lines = []
+
+    for line in response.splitlines():
+        if line.strip() == f'```{language}':
+            taking_lines = True
+            continue
+
+        if taking_lines and line.strip() == '```':
+            break
+
+        if taking_lines:
+            code_lines.append(line)
+
+    return '\n'.join(code_lines)
