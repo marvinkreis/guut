0e990174b0264ab7f4e2f0af67c02121028f75c5
Marvin Kreis <MarvinKreis@web.de>
2024-06-26 19:52:35 +0200

    Move prompts to their own folder

--- guut/prompts.py
+++ guut/prompts.py
@@ -1,4 +1,36 @@
 from pathlib import Path
 
-prompt_path = Path(__file__).parent.parent / 'prompt'
-prompt = prompt_path.read_text()
+from guut.llm import UserMessage, Message, FakeAssistantMessage
+
+prompts_path = Path(__file__).parent.parent / 'prompts'
+
+
+class LongInstructions:
+    def __init__(self):
+        self.content = (prompts_path / 'long_instructions').read_text()
+
+    def message(self) -> [Message]:
+        return UserMessage(self.content)
+
+
+class ShortInstructions:
+    def __init__(self):
+        self.content = (prompts_path / 'short_instructions').read_text()
+
+    def message(self) -> [Message]:
+        return UserMessage(self.content)
+
+
+class FewShotExample01:
+    def __init__(self):
+        self.paths = sorted(prompts_path.glob('example01__*__*'), key=lambda path: path.name)
+
+    @staticmethod
+    def _path_to_msg(path: Path):
+        if 'assistant' in path.name:
+            return FakeAssistantMessage(path.read_text())
+        else:
+            return UserMessage(path.read_text())
+
+    def messages(self):
+        return [FewShotExample01._path_to_msg(path) for path in self.paths]
