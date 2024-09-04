47b5e7369357b10f25f80678edc280f72ff45609
Marvin Kreis <MarvinKreis@web.de>
2024-09-04 15:58:11 +0200

    More tests

--- tests/loop_test.py
+++ tests/loop_test.py
@@ -1,9 +1,14 @@
 from functools import partial
+from pathlib import Path
+from unittest.mock import MagicMock
+
+import pytest
 
 from guut.dummy_problem import DummyProblem
 from guut.llm import AssistantMessage, Conversation, UserMessage
 from guut.llm_endpoints.replay_endpoint import ReplayLLMEndpoint
 from guut.loop import Loop, LoopSettings, State
+from guut.problem import ExecutionResult, TestResult, ValidationResult
 
 code_raw = """def test_something():
     print("something")"""
@@ -60,6 +65,8 @@ def test__observation_with_just_code_gets_detected():
     loop.perform_next_step()
     assert loop.get_state() == State.EXPERIMENT_RESULTS_GIVEN
     assert loop.experiments[0].description.kind == "observation"
+    assert code_raw in loop.experiments[0].description.code
+    assert loop.experiments[0].description.debugger_script is None
 
 
 def test__observation_with_only_debugger_script_leads_to_incomple_response():
@@ -69,22 +76,88 @@ def test__observation_with_only_debugger_script_leads_to_incomple_response():
 
     loop.perform_next_step()
     assert loop.get_state() == State.INCOMPLETE_RESPONSE
+
+
+def test__experiment_with_code_and_debugger_script_gets_detected():
+    conversation = Conversation([AssistantMessage("", tag=State.INITIAL)])
+    endpoint = ReplayLLMEndpoint.from_raw_messages([experiment(code, debugger_script)])
+    loop = Loop(endpoint=endpoint, conversation=conversation, problem=DummyProblem())
+
     loop.perform_next_step()
-    assert loop.get_state() == State.INCOMPLETE_RESPONSE_INSTRUCTIONS_GIVEN
+    assert loop.get_state() == State.EXPERIMENT_STATED
+    loop.perform_next_step()
+    assert loop.get_state() == State.EXPERIMENT_RESULTS_GIVEN
+    assert loop.experiments[0].description.kind == "experiment"
+    assert code_raw in loop.experiments[0].description.code
+    assert loop.experiments[0].description.debugger_script is not None
+    assert debugger_script_raw in loop.experiments[0].description.debugger_script
 
 
-def test__single_experiment_gets_detected():
+def test__experiment_with_just_code_gets_detected():
     conversation = Conversation([AssistantMessage("", tag=State.INITIAL)])
     endpoint = ReplayLLMEndpoint.from_raw_messages([experiment(code)])
     loop = Loop(endpoint=endpoint, conversation=conversation, problem=DummyProblem())
 
     loop.perform_next_step()
     assert loop.get_state() == State.EXPERIMENT_STATED
+    loop.perform_next_step()
+    assert loop.get_state() == State.EXPERIMENT_RESULTS_GIVEN
+    assert loop.experiments[0].description.kind == "experiment"
+    assert code_raw in loop.experiments[0].description.code
+    assert loop.experiments[0].description.debugger_script is None
 
 
-def test__single_test_gets_deteted():
+def test__experiment_with_only_debugger_script_leads_to_incomple_response():
+    conversation = Conversation([AssistantMessage("", tag=State.INITIAL)])
+    endpoint = ReplayLLMEndpoint.from_raw_messages([experiment(debugger_script)])
+    loop = Loop(endpoint=endpoint, conversation=conversation, problem=DummyProblem())
+
+    loop.perform_next_step()
+    assert loop.get_state() == State.INCOMPLETE_RESPONSE
+
+
+def test__test_with_code_and_debugger_script_gets_detected_but_debugger_script_gets_discarded():
+    conversation = Conversation([AssistantMessage("", tag=State.INITIAL)])
+    endpoint = ReplayLLMEndpoint.from_raw_messages([_test(code, debugger_script)])
+    loop = Loop(endpoint=endpoint, conversation=conversation, problem=DummyProblem())
+
+    loop.perform_next_step()
+    assert loop.get_state() == State.TEST_STATED
+    loop.perform_next_step()
+    assert loop.get_state() == State.TEST_DOESNT_DETECT_MUTANT  # TODO: mock problem
+    assert code_raw in loop.tests[0].description.code
+    assert not hasattr(loop.tests[0].description, "debugger_script")
+
+
+def test__test_with_just_code_gets_detected():
     conversation = Conversation([AssistantMessage("", tag=State.INITIAL)])
     endpoint = ReplayLLMEndpoint.from_raw_messages([_test(code)])
+    loop = Loop(endpoint=endpoint, conversation=conversation, problem=DummyProblem())
+
+    loop.perform_next_step()
+    assert loop.get_state() == State.TEST_STATED
+    loop.perform_next_step()
+    assert loop.get_state() == State.TEST_DOESNT_DETECT_MUTANT  # TODO: mock problem
+    assert code_raw in loop.tests[0].description.code
+    assert not hasattr(loop.tests[0].description, "debugger_script")
+
+
+def test__test_with_only_debugger_script_leads_to_incomple_response():
+    conversation = Conversation([AssistantMessage("", tag=State.INITIAL)])
+    endpoint = ReplayLLMEndpoint.from_raw_messages([_test(debugger_script)])
+    loop = Loop(endpoint=endpoint, conversation=conversation, problem=DummyProblem())
+
+    loop.perform_next_step()
+    assert loop.get_state() == State.INCOMPLETE_RESPONSE
+
+
+def test__experiment_is_preferred_over_observation_when_first():
+    conversation = Conversation(
+        [
+            AssistantMessage("", tag=State.INITIAL),
+        ]
+    )
+    endpoint = ReplayLLMEndpoint.from_raw_messages([f"{experiment(code)}\n\n{observation(code)}\n\n{experiment(code)}"])
     loop = Loop(
         endpoint=endpoint,
         conversation=conversation,
@@ -92,10 +165,10 @@ def test__single_test_gets_deteted():
     )
 
     loop.perform_next_step()
-    assert loop.get_state() == State.TEST_STATED
+    assert loop.get_state() == State.EXPERIMENT_STATED
 
 
-def test__test_is_preferred_over_experiment_if_no_test_instructions_were_given():
+def test__test_is_preferred_over_experiment_if_no_header_is_present_and_test_instructions_werent_given_yet():
     conversation = Conversation([AssistantMessage("", tag=State.INITIAL)])
     endpoint = ReplayLLMEndpoint.from_raw_messages([f"{experiment(code)}\n\n{_test(code)}"])
     loop = Loop(
@@ -108,7 +181,7 @@ def test__test_is_preferred_over_experiment_if_no_test_instructions_were_given()
     assert loop.get_state() == State.TEST_STATED
 
 
-def test__test_is_preferred_over_experiment_if_test_instructions_were_given():
+def test__test_is_preferred_over_experiment_if_no_header_is_present_and_test_instructions_were_already_given():
     conversation = Conversation(
         [
             AssistantMessage("", tag=State.INITIAL),
@@ -212,6 +285,184 @@ def test__conversation_is_aborted_if_an_experiment_beyond_the_max_is_submitted()
     assert loop.get_state() == State.ABORTED
 
 
-# TODO: incomplete responses and max
+def test__test_instructions_are_given_after_max_experiments_are_reached_and_a_test_was_already_submitted():
+    exp_msgs = [
+        UserMessage(experiment(code), tag=State.EXPERIMENT_STATED),
+        UserMessage("", tag=State.EXPERIMENT_RESULTS_GIVEN),
+    ]
 
-# ==============================================================================================
+    conversation = Conversation(
+        [
+            AssistantMessage("", tag=State.INITIAL),
+            *(exp_msgs * 4),
+            UserMessage(experiment(code), tag=State.TEST_STATED),
+            UserMessage("", tag=State.TEST_DOESNT_DETECT_MUTANT),
+            *(exp_msgs * 4),
+        ]
+    )
+    endpoint = ReplayLLMEndpoint.from_raw_messages([experiment(code)] * 2)
+    loop = Loop(
+        endpoint=endpoint,
+        conversation=conversation,
+        problem=DummyProblem(),
+        settings=LoopSettings(max_num_experiments=10),
+    )
+
+    loop.perform_next_step()
+    assert loop.get_state() == State.EXPERIMENT_STATED
+    loop.perform_next_step()
+    assert loop.get_state() == State.EXPERIMENT_RESULTS_GIVEN
+    loop.perform_next_step()
+    assert loop.get_state() == State.EXPERIMENT_STATED
+    loop.perform_next_step()
+    assert loop.get_state() == State.TEST_INSTRUCTIONS_GIVEN
+
+
+def test__incomplete_response_instructions_are_given_after_incomplete_response():
+    conversation = Conversation([AssistantMessage("", tag=State.INCOMPLETE_RESPONSE)])
+    endpoint = ReplayLLMEndpoint.from_raw_messages([])
+    loop = Loop(endpoint=endpoint, conversation=conversation, problem=DummyProblem())
+
+    loop.perform_next_step()
+    assert loop.get_state() == State.INCOMPLETE_RESPONSE_INSTRUCTIONS_GIVEN
+
+
+def test__conversation_is_continued_with_experiment_after_incomplete_response():
+    conversation = Conversation([AssistantMessage("", tag=State.INCOMPLETE_RESPONSE_INSTRUCTIONS_GIVEN)])
+    endpoint = ReplayLLMEndpoint.from_raw_messages([experiment(code)])
+    loop = Loop(
+        endpoint=endpoint,
+        conversation=conversation,
+        problem=DummyProblem(),
+    )
+
+    loop.perform_next_step()
+    assert loop.get_state() == State.EXPERIMENT_STATED
+
+
+def test__conversation_is_continued_with_test_after_incomplete_response():
+    conversation = Conversation([AssistantMessage("", tag=State.INCOMPLETE_RESPONSE_INSTRUCTIONS_GIVEN)])
+    endpoint = ReplayLLMEndpoint.from_raw_messages([_test(code)])
+    loop = Loop(
+        endpoint=endpoint,
+        conversation=conversation,
+        problem=DummyProblem(),
+    )
+
+    loop.perform_next_step()
+    assert loop.get_state() == State.TEST_STATED
+
+
+def test__conversation_is_aborted_if_an_incomplete_response_beyond_the_max_is_submitted():
+    conversation = Conversation([AssistantMessage("", tag=State.INITIAL)])
+    endpoint = ReplayLLMEndpoint.from_raw_messages([experiment(debugger_script)] * 3)
+    loop = Loop(
+        endpoint=endpoint,
+        conversation=conversation,
+        problem=DummyProblem(),
+        settings=LoopSettings(max_num_incomplete_responses=2),
+    )
+
+    loop.perform_next_step()
+    assert loop.get_state() == State.INCOMPLETE_RESPONSE
+    loop.perform_next_step()
+    assert loop.get_state() == State.INCOMPLETE_RESPONSE_INSTRUCTIONS_GIVEN
+    loop.perform_next_step()
+    assert loop.get_state() == State.INCOMPLETE_RESPONSE
+    loop.perform_next_step()
+    assert loop.get_state() == State.INCOMPLETE_RESPONSE_INSTRUCTIONS_GIVEN
+    loop.perform_next_step()
+    assert loop.get_state() == State.INCOMPLETE_RESPONSE
+    loop.perform_next_step()
+    assert loop.get_state() == State.ABORTED
+
+
+def test__experiment_doesnt_compile():
+    conversation = Conversation([AssistantMessage("", tag=State.INITIAL)])
+    endpoint = ReplayLLMEndpoint.from_raw_messages([experiment(code)])
+
+    problem = DummyProblem()
+    problem.validate_code = MagicMock(return_value=ValidationResult(valid=False, error=""))
+
+    loop = Loop(
+        endpoint=endpoint,
+        conversation=conversation,
+        problem=problem,
+    )
+
+    loop.perform_next_step()
+    assert loop.get_state() == State.EXPERIMENT_STATED
+    loop.perform_next_step()
+    assert loop.get_state() == State.EXPERIMENT_DOESNT_COMPILE
+
+
+def test__test_doesnt_compile():
+    conversation = Conversation([AssistantMessage("", tag=State.INITIAL)])
+    endpoint = ReplayLLMEndpoint.from_raw_messages([_test(code)])
+
+    problem = DummyProblem()
+    problem.validate_code = MagicMock(return_value=ValidationResult(valid=False, error=""))
+
+    loop = Loop(
+        endpoint=endpoint,
+        conversation=conversation,
+        problem=problem,
+    )
+
+    loop.perform_next_step()
+    assert loop.get_state() == State.TEST_STATED
+    loop.perform_next_step()
+    assert loop.get_state() == State.TEST_DOESNT_COMPILE
+
+
+@pytest.mark.parametrize(argnames=["correct_exit_code", "mutant_exit_code"], argvalues=[(0, 0), (1, 1), (1, 0)])
+def test__test_doesnt_detect_mutant(correct_exit_code, mutant_exit_code):
+    conversation = Conversation([AssistantMessage("", tag=State.INITIAL)])
+    endpoint = ReplayLLMEndpoint.from_raw_messages([_test(code)])
+
+    problem = DummyProblem()
+    problem.run_test = MagicMock(
+        return_value=TestResult(
+            correct=ExecutionResult(
+                input="", args=[], cwd=Path("."), output="", target=Path("."), exitcode=correct_exit_code
+            ),
+            mutant=ExecutionResult(
+                input="", args=[], cwd=Path("."), output="", target=Path("."), exitcode=mutant_exit_code
+            ),
+        )
+    )
+
+    loop = Loop(
+        endpoint=endpoint,
+        conversation=conversation,
+        problem=problem,
+    )
+
+    loop.perform_next_step()
+    assert loop.get_state() == State.TEST_STATED
+    loop.perform_next_step()
+    assert loop.get_state() == State.TEST_DOESNT_DETECT_MUTANT
+
+
+def test__response_when_successful_test():
+    conversation = Conversation([AssistantMessage("", tag=State.INITIAL)])
+    endpoint = ReplayLLMEndpoint.from_raw_messages([_test(code)])
+
+    problem = DummyProblem()
+    problem.run_test = MagicMock(
+        return_value=TestResult(
+            correct=ExecutionResult(input="", args=[], cwd=Path("."), output="", target=Path("."), exitcode=0),
+            mutant=ExecutionResult(input="", args=[], cwd=Path("."), output="", target=Path("."), exitcode=1),
+        )
+    )
+
+    loop = Loop(
+        endpoint=endpoint,
+        conversation=conversation,
+        problem=problem,
+    )
+
+    loop.perform_next_step()
+    assert loop.get_state() == State.TEST_STATED
+    loop.perform_next_step()
+    assert loop.get_state() == State.DONE
