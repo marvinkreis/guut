979af9884faa324b66fa3179a07b418b12384a97
Marvin Kreis <MarvinKreis@web.de>
2024-09-16 18:24:40 +0200

    Supress output from coverage library

--- guut/util.py
+++ guut/util.py
@@ -1,18 +1,18 @@
 from pathlib import Path
-from subprocess import Popen
+from subprocess import DEVNULL, Popen
 
 from loguru import logger
 
 
 def ensure_python_coverage_module_is_installed(python_interpreter: Path):
     check_command = [str(python_interpreter), "-m", "coverage"]
-    check_process = Popen(check_command)
+    check_process = Popen(check_command, stdout=DEVNULL, stderr=DEVNULL)
     check_process.wait()
 
     if check_process.returncode != 0:
         logger.info(f"Coverage module is not installed. Installing 'coverage' for '{python_interpreter}'...")
         install_command = [str(python_interpreter), "-m", "pip", "install", "coverage"]
-        install_process = Popen(install_command)
+        install_process = Popen(install_command, stdout=DEVNULL, stderr=DEVNULL)
         install_process.wait()
 
         if install_process.returncode != 0:
